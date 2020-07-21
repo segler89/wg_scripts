053d427652eb96252388a4393589babdb2034b4c
Domi <42076024+segler89@users.noreply.github.com>
2020-07-21 13:02:50 +0200

    Update clearfolder.sh

--- clearfolder.sh
+++ clearfolder.sh
@@ -2,6 +2,4 @@
 
 sudo mv "$1"_private_key "$1"_public_key /etc/wireguard/keys/peerkeys/
 sudo mv "$1".conf /etc/wireguard/clientconfs/
-rm tmp tmp1 tmp2 tmp3
-[ -e tm ] && rm tm
-[ -e tmp4 ] && rm tmp4
+rm tmp* tm &> /dev/null/
