408f5020036fbb2ffa4c125586c9213645dd6691
Domi <42076024+segler89@users.noreply.github.com>
2020-07-21 13:13:47 +0200

    Update wg_firstclientconf.sh

--- wg_firstclientconf.sh
+++ wg_firstclientconf.sh
@@ -5,7 +5,7 @@
 
 if ! [ -d /etc/wireguard/keys/peerkeys/ ]
 then
-	sudo mkdir /etc/wireguard/keys/peerkeys/
+	sudo mkdir -p /etc/wireguard/keys/peerkeys/
 fi
 if ! [ -d /etc/wireguard/clientconfs/ ]
 then
@@ -16,24 +16,12 @@ fi
 #Generating the keys in the current folder:
 wg genkey | tee "$1"_private_key | wg pubkey > "$1"_public_key
 
-# Create the next IP-Adress.
-N=$(sudo head -n 2 /etc/wireguard/wg0.conf)
-echo "$N" > tm
-NO=$(sudo tail -n1 tm)
-echo ${NO##*.} > tmp
-NOO=$(cat tmp)
-echo ${NOO%/*} > tmp1
-echo $[1 + $(cat tmp1)] > tmp2
-NOOO=$(cat tmp2)
-IPADDR="10.10.10.${NOOO}/32"
-echo "$IPADDR" > tmp3
-
 PRIVKEY=$(cat "$1"_private_key)
 
-
+# Make the clientconf-file:
 echo "[Interface]
 	PrivateKey = ${PRIVKEY}
-	Address = $(cat tmp3)
+	Address = 10.10.10.1/32
 	DNS = 192.168.178.27
 
       [Peer]
